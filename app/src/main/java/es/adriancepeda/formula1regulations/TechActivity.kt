65373bdf5fb87d7cf2822acd9c4d8642844ea0cb
Adrián Cepeda Ávila <adriancepeda1310@gmail.com>
2020-07-24 21:02:39 +0200

    Experiments and first tests

--- app/src/main/java/es/adriancepeda/formula1regulations/TechActivity.kt
+++ app/src/main/java/es/adriancepeda/formula1regulations/TechActivity.kt
@@ -1,12 +1,19 @@
 package es.adriancepeda.formula1regulations
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TechActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTitle(R.string.techTitle)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tech)
+        val buttonActual : Button = findViewById(R.id.techActualYear)
+        buttonActual.setOnClickListener{
+            val intent= Intent(this, TechActivity20::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/es/adriancepeda/formula1regulations/TechActivity20.kt
@@ -0,0 +1,48 @@
+package es.adriancepeda.formula1regulations
+
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
+import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toolbar
+import androidx.annotation.RequiresApi
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.widget.NestedScrollView
+
+class TechActivity20 : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tech20)
+        val content: LinearLayout =findViewById(R.id.scrollTech20)
+        val text=resources.getStringArray(R.array.tech_20)
+        val title: Toolbar = Toolbar(this).also {
+            it.setTitle(R.string.techTitle20)
+            it.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary))
+            it.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        }
+        content.addView(title)
+        val textViewList=Array(text.size){}
+        for (i in text.indices){
+            val article=text[i].split("%s")
+            val textView:TextView= TextView(this)
+            textView.setText((i+1).toString()+". "+article[0])
+            textView.setPadding(15,15,15,15)
+            textView.setOnClickListener{
+                val intent= Intent(this, MainActivity::class.java)
+                intent.putExtra("text", text[i])
+                startActivity(intent)
+            }
+            content.addView(textView)
+        }
+    }
+}
